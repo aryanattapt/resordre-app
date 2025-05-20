8b5f060ee9362ea90f7c62fa9a7ff57f8f8c0d78
Aryanatta P.T <aryanattapt28@outlook.co.id>
2025-05-20 19:36:27 +0700

    update jwt lib

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,24 +1,33 @@
 import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = process.env.SECRET_KEY || '';
 
 interface Payload {
     [key: string]: any;
 }
 
-export const signToken = (payload: Payload, expiresIn: string | number = '1h'): string => {
-    try {
-        const token = jwt.sign(payload, SECRET_KEY, { expiresIn });
-        return token;
-    } catch (error) {
-        console.error('Error signing token', error);
-        throw new Error('Could not sign the token');
-    }
+if (!SECRET_KEY) {
+  throw new Error('JWT_SECRET is not defined');
+}
+
+export const signToken = (payload: object, expiresIn: number = 3600): string => {
+  try {
+    const token = jwt.sign(payload, SECRET_KEY, { 
+        expiresIn: expiresIn,
+        algorithm: "ES256"
+    });
+    return token;
+  } catch (error) {
+    console.error('Error signing token', error);
+    throw new Error('Could not sign the token');
+  }
 };
 
 export const verifyToken = (token: string): Payload => {
     try {
-        const decoded = jwt.verify(token, SECRET_KEY) as Payload;
+        const decoded = jwt.verify(token, SECRET_KEY, {
+            algorithms: ['ES256']
+        }) as Payload;
         return decoded;
     } catch (error) {
         console.error('Error verifying token', error);
